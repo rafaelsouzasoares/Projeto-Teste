c47e58ae5dcbda7141af16187dc9a04e6bc46903
rafaelsouzasoares <rafaelsouzasoares2@gmail.com>
2017-09-13 09:19:05 -0300

    Incluindo arquivos do programa de teste
    Programa de teste

---
+++ Integracao/Integracao/IntegrarDados.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Threading;
+
+namespace Integracao
+{
+    public class IntegrarDados
+    {
+        IWebDriver driver = new ChromeDriver();
+        
+        [Test]
+        public void integrar()
+        {
+            //Setando a url de destino
+            driver.Navigate().GoToUrl("https://esgcorpfirm.novajus.com.br");
+
+            //Instanciando a classe que esperará por até 40 segundos por algum elemento
+            WebDriverWait espera = new WebDriverWait(driver, TimeSpan.FromMinutes(5));
+
+            //Verificando se o elemento já está visivel e habilitado para acessar a tela de login
+            espera.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("btn-login-onepass")));
+
+            //Selecionando o modo de login para OnePass
+            if (driver.FindElements(By.Id("btn-login-onepass")).Count > 0)
+            {
+                driver.FindElement(By.Id("btn-login-onepass")).Click();
+            }
+
+
+            //Passando dados de login - Usuário e senha
+            if (driver.FindElements(By.Id("Username")).Count > 0)
+            {
+                driver.FindElement(By.Id("Username")).SendKeys("esgcorpfirm.teste03");
+            }
+
+            if (driver.FindElements(By.Id("Password")).Count > 0)
+            {
+                driver.FindElement(By.Id("Password")).SendKeys("Esg@2017");
+            }
+
+            //Aguardando botão de login já está visível e habilitado para uso
+            espera.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("SignIn")));
+
+            Thread.Sleep(6000);
+
+            if (driver.FindElements(By.Id("SignIn")).Count > 0)
+            {
+                driver.FindElement(By.Id("SignIn")).Submit();
+            }
+            
+        }
+
+
+
+        //Método responsável por pesquisar os contatos buscados da planilha
+        public void Pesquisar(string nomeContato, string cpf)
+        {
+            //Instanciando a classe que esperará por até 40 segundos por algum elemento
+            WebDriverWait espera = new WebDriverWait(driver, TimeSpan.FromMinutes(5));
+
+            //Verificando se o elemento já está visivel e habilitado para acessar a tela de de Contatos
+            espera.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("menucontatos")));
+
+            if (driver.FindElements(By.LinkText("Contatos")).Count > 0)
+            {
+                driver.FindElement(By.LinkText("Contatos")).Click();
+            }
+
+            espera.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.Id("Search")));
+
+            if (driver.FindElements(By.Id("Search")).Count > 0)
+            {
+                driver.FindElement(By.Id("Search")).Clear();
+                driver.FindElement(By.Id("Search")).SendKeys(cpf);
+            }
+
+            if (driver.FindElements(By.ClassName("button")).Count > 0)
+            {
+                driver.FindElement(By.ClassName("button")).Click();
+            }
+
+            Thread.Sleep(10000);
+
+            //Caso essa condição seja satisfeita quer dizer que não encontrou nenhum registro na pesquisa
+           if(driver.FindElements(By.ClassName("empty-search-box")).Count > 0 )
+            {
+                Cadastrar(nomeContato, cpf);               
+            }                    
+
+        }
+
+
+        //Método responsável por inserir os contatos que não existirem
+        public void Cadastrar(String nomeContato, string cpf)
+        {
+
+            //Instanciando a classe que esperará por até 40 segundos por algum elemento
+            WebDriverWait espera = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+
+            
+            //Chamar tela de cadastro de contato
+            if (driver.FindElements(By.LinkText("Nova pessoa física")).Count > 0)
+            {
+                driver.FindElement(By.LinkText("Nova pessoa física")).Click();
+            }
+            Thread.Sleep(6000);
+
+            if (driver.FindElements(By.Id("CPF")).Count > 0)
+            {
+                driver.FindElement(By.Id("CPF")).SendKeys(cpf);
+            }
+            
+            if (driver.FindElements(By.Id("Nome")).Count > 0)
+            {
+                driver.FindElement(By.Id("Nome")).SendKeys(nomeContato);
+            }
+
+            if (driver.FindElements(By.Name("ButtonSave")).Count > 0)
+            {
+                driver.FindElement(By.Name("ButtonSave")).Click();
+            }
+
+            //Aguardando mensagem de contato incluído com sucesso para continuar
+            espera.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("top-message-body sucesso")));
+                        
+        }
+
+        public void Finalizar()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ ProgramaTeste/ProgramaTeste/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using excel = Microsoft.Office.Interop.Excel;
+using OpenQA.Selenium.Support.UI;
+using Integracao;
+
+namespace ProgramaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            excel.Application x1app = new excel.Application();
+
+            excel.Workbook x1workbook = x1app.Workbooks.Open(@"C:\Users\Rafael\Desktop\origem de dados.xlsx");
+
+            //Pegando a pasta de trabalho do excel onde estão os dados
+            excel.Worksheet x1worksheet = x1workbook.Sheets[1];
+
+            //Obtendo o valor das linhas do arquivo
+            excel.Range x1range = x1worksheet.UsedRange;
+
+            int numLinhas = x1range.Cells.Rows.Count;
+            String nomeUsuario;
+            String cpf;
+
+            //Iniciando processo de passagem de dados para integração
+            Integracao.IntegrarDados integracao = new Integracao.IntegrarDados();
+
+            integracao.integrar();
+
+            for (int i = 2000; i <= numLinhas; i++)
+            {
+                nomeUsuario = (x1range.Cells[i, 1] as excel.Range).Value.ToString();
+                cpf = (x1range.Cells[i, 2] as excel.Range).Value.ToString();
+
+                integracao.Pesquisar(nomeUsuario, cpf);
+            }
+            //Finalizando ChromeWebDrive
+            integracao.Finalizar();
+
+
+            Console.WriteLine("Processo finalizado!");
+            Environment.Exit(0);
+
+                        
+        }
+    }
+}
